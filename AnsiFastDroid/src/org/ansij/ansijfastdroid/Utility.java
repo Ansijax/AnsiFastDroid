c7cecf1fb88b755cfe8b41053b46927f494981b7
Ansijax <ansijax@gmail.com>
2013-03-10 19:41:38 +0100

    first commit

---
+++ AnsiFastDroid/src/org/ansij/ansijfastdroid/Utility.java
@@ -0,0 +1,18 @@
+package org.ansij.ansijfastdroid;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Utility {
+	
+	public static boolean isOnline(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
+	
+}
